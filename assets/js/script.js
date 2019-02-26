ff1605929a1f2f3f9f3463266d0d5a1081cb8917
emilie bialais <emiliebialais@MacBookAir.local>
2019-02-26 14:45:45 +0100

    avant branch dev

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    // let nav = document.querySelector('nav');
+    // let ul = document.createElement('ul');
+    // nav.appendChild(ul);
+
+    // let a = document.createElement('a');
+    // ul.appendChild(a);
+    // ul.appendChild(a);
+    // ul.appendChild(a);
+    // ul.appendChild(a);
+
+
+
+}
