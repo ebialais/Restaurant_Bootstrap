3e45aea6dc87591e63cccd271d3faeae3bf71fdb
emilie bialais <emiliebialais@MacBookAir.local>
2019-02-28 10:21:34 +0100

    script prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-// let user = document.getElementById("utilisateur");
-// let nom = prompt("Bonjour, quel est ton nom ?");
-
-// affiche = function (a){
-//     user.innerHTML = a;
-// }
+let nom = prompt('Bonjour, quel est votre nom ? ', ' ');
 
 
-// window.onload = function() {
-//     affiche(nom)
+if ( (nom == ' ') || (nom == null) ) {
+    nom = "Bob";
+    }
+
+//document.write("<H2>Bonjour " + nom + " ! Bienvenu(e) au Bistro du coin. </H2>");
+
+window.onload = function () {
+    let user = document.getElementById("utilisateur");
+    user.innerHTML = nom;
+}
 
 
-// }
